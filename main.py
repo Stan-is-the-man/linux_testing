b6d8d8d637dc18c8e988f9ffc8f50f3f637d56e3
Stanislav Yanakiev <stanislav.yanakievv@gmail.com>
2023-02-14 13:28:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello Linux")
+command = input()
+students = {}
+
+while ":" in command:
+    split = command.split(':')
+    name = split[0]
+    points = int(split[1])
+    course = split[2]
+    students[name] = [points, course]
+    
+    command = input()
+
+for name, points in students.items():
+    if command == students[name][1]:
+        print(f"{name} - {points[0]}")
+
+print(students)
